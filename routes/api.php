de3b7a501083a82f1725d966837649ee68cc3032
nanankhidayat <nanankhidayat@gmail.com>
2020-09-25 14:25:54 +0700

    finish routes tutorial

--- routes/api.php
+++ routes/api.php
@@ -22,6 +22,34 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 //     return 'Hi User';
 // });
 
-Route::get('/user/{name}', function ($name) {
-    return 'Hi ' . $name;
-});
+// Route::get('/user/{name}', function ($name) {
+//     return 'Hi ' . $name;
+// }); -- route with parameter
+
+// Route::get('/user/{name}', function ($name = null) {
+//     return 'Hi ' . $name;
+// }); -- route without blank paramarer is gone be empty
+
+// Route::get('/user/{name}', function ($name = null) {
+//     return 'Hi ' . $name;
+// })->where('name', '[a-zA-Z]+'); -- route with permison alpabeth only
+
+// Route::get('/product/{id}', function ($id) {
+//     return 'Product id is ' . $id;
+// })->where('id', '[0-9]+'); -- route with permison number only
+
+// Route::get('/user/{name}', function ($name = null) {
+//     return 'Hi ' . $name;
+// }); --Route with rulles constraints in routeServiceProvider
+
+// Route::get('/product/{id}', function ($id) {
+//     return 'Product id is ' . $id;
+// });  --Route with rulles constraints in routeServiceProvider
+
+// Route::match(['get', 'post'], '/students', function (Request $req) {
+//     return 'Requested method is ' . $req->method();
+// });  --Route with multi request
+
+// Route::any('/posts', function (Request $req) {
+//     return 'Requested method is ' . $req->method();
+// }); -- Route with all request
